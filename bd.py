8cce2b08b0a3c1987098111ba12375ffd432e3e7
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2023-03-26 00:04:08 -0400

    Corrections in Webpage

--- bd.py
+++ bd.py
@@ -3,5 +3,5 @@ import pymysql
 def get_information():
     return pymysql.connect(host="Maps182.mysql.pythonanywhere-services.com",
                           user="Maps182",
-                          password="M@ps182Register",
-                          db="Registration")
+                          password="M@ps18244Registration",
+                          db="Maps182$Registration")
